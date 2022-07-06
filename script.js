339727ad00a5d5f378ed16d80f11a2830d67d01a
ademonuroztas <ademonuroztas@gmail.com>
2022-07-06 04:26:37 +0300

    Create playRound function

--- script.js
+++ script.js
@@ -4,4 +4,20 @@ function computerPlay() {
     return randomChoice;
   }
 
-console.log(computerPlay());
+
+  function playRound(playerSelection, computerSelection) {
+    if(playerSelection === computerSelection) {
+        console.log("It's a draw.");
+        return "draw"
+    } else if((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+        return playerSelection;
+    } else {
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+        return computerSelection;
+    }
+  }
+  
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
