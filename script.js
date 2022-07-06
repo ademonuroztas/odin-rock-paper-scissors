f247e07b1f09a676d86b9f905f156c1cb1b86c27
ademonuroztas <ademonuroztas@gmail.com>
2022-07-06 05:19:22 +0300

    Fixed catching invalid inputs bug in playRound function

--- script.js
+++ script.js
@@ -6,26 +6,30 @@ function computerPlay() {
 
 
   function playRound(playerSelection, computerSelection) {
-    if(playerSelection === computerSelection) {
-        console.log("It's a draw.");
-        return "draw"
-    } else if((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
-        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
-        return playerSelection;
+    if(playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
+        if(playerSelection === computerSelection) {
+            console.log("It's a draw.");
+            return "draw"
+        } else if((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+            return playerSelection;
+        } else {
+            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+            return computerSelection;
+        }
     } else {
-        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-        return computerSelection;
+        console.log("Invalid input. Please select one of three: Rock | Paper | Scissors")
+        return "error";
     }
   }
 
   function game() {
     
     for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("What is your choice?").toLowerCase();
         const computerSelection = computerPlay();
         playRound(playerSelection, computerSelection)
      }
   }
   
-  const playerSelection = "rock";
-
   game();
