424fb19a4122fd985c7aa0e2562e84326b6b64dd
ademonuroztas <ademonuroztas@gmail.com>
2022-07-06 03:50:58 +0300

    Create computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Alert!");
+function computerPlay() {
+    const choices = ["rock", "paper", "scissors"];
+    var randomChoice = choices[Math.floor(Math.random()*choices.length)];
+    return randomChoice;
+  }
+
+console.log(computerPlay());
