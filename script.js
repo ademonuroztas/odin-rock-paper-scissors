d76432b3077282da2700c79a1d40ca705cbf6b13
ademonuroztas <ademonuroztas@gmail.com>
2022-07-23 17:47:20 +0300

    Add UI

--- script.js
+++ script.js
@@ -1,36 +1,64 @@
+let playerScore = 0;
+let computerScore = 0;
+let round = 1;
+
+const playerScoreText = document.querySelector('#player-score');
+const computerScoreText = document.querySelector('#computer-score');
+const winner = document.querySelector('#winner');
+
+
 function computerPlay() {
-    const choices = ["rock", "paper", "scissors"];
-    var randomChoice = choices[Math.floor(Math.random()*choices.length)];
-    return randomChoice;
-  }
+  const choices = ['rock', 'paper', 'scissors'];
+  var randomChoice = choices[Math.floor(Math.random() * choices.length)];
+  return randomChoice;
+}
 
-
-  function playRound(playerSelection, computerSelection) {
-    if(playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
-        if(playerSelection === computerSelection) {
-            console.log("It's a draw.");
-            return "draw"
-        } else if((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
-            console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
-            return playerSelection;
-        } else {
-            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-            return computerSelection;
-        }
+function playRound(playerSelection, computerSelection) {
+  if (
+    playerSelection === 'rock' ||
+    playerSelection === 'paper' ||
+    playerSelection === 'scissors'
+  ) {
+    if (playerSelection === computerSelection) {
+      return "It's a draw.";
+    } else if (
+      (playerSelection === 'rock' && computerSelection === 'scissors') ||
+      (playerSelection === 'paper' && computerSelection === 'rock') ||
+      (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+      playerScore++;
+      playerScoreText.innerHTML = `Your score: ${playerScore}`;
+      return `You Win! ${playerSelection} beats ${computerSelection}`;
     } else {
-        console.log("Invalid input. Please select one of three: Rock | Paper | Scissors")
-        return "error";
+      computerScore++;
+      computerScoreText.innerHTML = `Computer score: ${computerScore}`;
+      return `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
+  } else {
+    return 'Invalid input. Please select one of three: Rock | Paper | Scissors';
   }
+}
 
-  function game() {
-    
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("What is your choice?");
-        playerSelection = playerSelection.toLowerCase();
-        const computerSelection = computerPlay();
-        playRound(playerSelection, computerSelection);
-     }
-  }
-  
-  game();
+const container = document.querySelector('#container');
+
+const buttons = Array.from(document.querySelectorAll('.button'));
+buttons.forEach((button) =>
+  button.addEventListener('click', function () {
+    if(computerScore < 5 && playerScore < 5) { 
+      const h3 = document.createElement('h3');
+      h3.textContent = `Round ${round++}: ${playRound(button.dataset.choice, computerPlay())}`;
+      container.appendChild(h3);
+    } else {
+      const h1 = document.createElement('h1');
+      if(playerScore > computerScore){
+        h1.textContent = `You win the game!!!`;
+      } else if (computerScore > playerScore) {
+        h1.textContent = `Computer wins the game.`;
+      } else {
+        h1.textContent = `Draw.`;
+      }
+      winner.appendChild(h1);
+    }
+  })
+);
+
